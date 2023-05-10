93291fb76fd63533b8e7bb2d6f65110561047341
zilian <zilian@microsoft.com>
2023-05-10 13:50:50 +0800

    Flask can run. Logging can run

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
 import os
+import time
+import logging
+import openai
+from azure.identity import DefaultAzureCredential
+# from azure.search.documents import SearchClient
+# from app.chatreadretrieveread import ChatReadRetrieveReadApproach
+# from azure.storage.blob import BlobServiceClient
 
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for, jsonify)
+
+
+# Replace these with your own values, either in environment variables or directly here
+AZURE_STORAGE_ACCOUNT = os.environ.get("AZURE_STORAGE_ACCOUNT") or "mystorageaccount"
+AZURE_STORAGE_CONTAINER = os.environ.get("AZURE_STORAGE_CONTAINER") or "content"
+AZURE_SEARCH_SERVICE = os.environ.get("AZURE_SEARCH_SERVICE") or "gptkb"
+AZURE_SEARCH_INDEX = os.environ.get("AZURE_SEARCH_INDEX") or "gptkbindex"
+AZURE_OPENAI_SERVICE = os.environ.get("AZURE_OPENAI_SERVICE") or "myopenai"
+AZURE_OPENAI_GPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_GPT_DEPLOYMENT") or "davinci"
+AZURE_OPENAI_CHATGPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_CHATGPT_DEPLOYMENT") or "chat"
+
+KB_FIELDS_CONTENT = os.environ.get("KB_FIELDS_CONTENT") or "content"
+KB_FIELDS_CATEGORY = os.environ.get("KB_FIELDS_CATEGORY") or "category"
+KB_FIELDS_SOURCEPAGE = os.environ.get("KB_FIELDS_SOURCEPAGE") or "sourcepage"
+
+
+# Use the current user identity to authenticate with Azure OpenAI, Cognitive Search and Blob Storage (no secrets needed, 
+# just use 'az login' locally, and managed identity when deployed on Azure). If you need to use keys, use separate AzureKeyCredential instances with the 
+# keys for each service
+# If you encounter a blocking error during a DefaultAzureCredntial resolution, you can exclude the problematic credential by using a parameter (ex. exclude_shared_token_cache_credential=True)
+azure_credential = DefaultAzureCredential()
+
+# Used by the OpenAI SDK
+openai.api_type = "azure"
+openai.api_base = f"https://{AZURE_OPENAI_SERVICE}.openai.azure.com"
+openai.api_version = "2022-12-01"
+
+# Comment these two lines out if using keys, set your API key in the OPENAI_API_KEY environment variable instead
+openai.api_type = "azure_ad"
+openai_token = azure_credential.get_token("https://cognitiveservices.azure.com/.default")
+openai.api_key = openai_token.token
 
 app = Flask(__name__)
-
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def index():
@@ -18,25 +56,14 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-   
-@app.route("/chat", methods=["POST"])
-def chat():
     ensure_openai_token()
     approach = request.json["approach"]
     try:
-        impl = chat_approaches.get(approach)
-        if not impl:
-            return jsonify({"error": "unknown approach"}), 400
-        r = impl.run(request.json["history"], request.json.get("overrides") or {})
-        return jsonify(r)
+        # impl = chat_approaches.get(approach)
+        # if not impl:
+        #     return jsonify({"error": "unknown approach"}), 400
+        # r = impl.run(request.json["history"], request.json.get("overrides") or {})
+        return jsonify('sth', 200)
     except Exception as e:
         logging.exception("Exception in /chat")
         return jsonify({"error": str(e)}), 500
@@ -46,7 +73,7 @@ def ensure_openai_token():
     if openai_token.expires_on < int(time.time()) - 60:
         openai_token = azure_credential.get_token("https://cognitiveservices.azure.com/.default")
         openai.api_key = openai_token.token
-
+        app.logger.info("OpenAI token at %s", openai_token)
 
 if __name__ == '__main__':
    app.run()
