dc20324dff4528539966cf7bf0b083bf1aca7e8d
zilian <zilian@microsoft.com>
2023-05-10 11:55:30 +0800

    Added npm react

--- app.py
+++ app.py
@@ -26,6 +26,26 @@ def hello():
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
+   
+@app.route("/chat", methods=["POST"])
+def chat():
+    ensure_openai_token()
+    approach = request.json["approach"]
+    try:
+        impl = chat_approaches.get(approach)
+        if not impl:
+            return jsonify({"error": "unknown approach"}), 400
+        r = impl.run(request.json["history"], request.json.get("overrides") or {})
+        return jsonify(r)
+    except Exception as e:
+        logging.exception("Exception in /chat")
+        return jsonify({"error": str(e)}), 500
+
+def ensure_openai_token():
+    global openai_token
+    if openai_token.expires_on < int(time.time()) - 60:
+        openai_token = azure_credential.get_token("https://cognitiveservices.azure.com/.default")
+        openai.api_key = openai_token.token
 
 
 if __name__ == '__main__':
